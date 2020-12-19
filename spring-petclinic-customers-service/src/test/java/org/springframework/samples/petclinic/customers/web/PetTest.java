4d41511170ad4e276e046266eb475390ee435061
asimsek29 <asimsek29@gmail.com>
2020-12-19 18:25:32 +0000

    added changes

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetTest.java
@@ -20,7 +20,7 @@ public class PetTest {
         //Arrange
         Pet pet = new Pet();
         Owner owner = new Owner();
-        owner.setFirstName("Call");
+        owner.setFirstName("Adam");
         //Act
         pet.setOwner(owner);
         //Assert
